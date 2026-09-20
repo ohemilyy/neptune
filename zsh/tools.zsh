92a91916d0d8479ff2c74242f0c9f79e00d10b47
Emily Gurnee <luna@pawful.dev>
2026-09-20 01:40:06 -0400

    fix: show LunaFetch in fresh terminal windows

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -8,7 +8,6 @@ if (( $+commands[starship] )); then
   eval "$(starship init zsh)"
 fi
 
-if (( $+commands[lunafetch] )) && [[ -o interactive && $SHLVL -eq 1 && -z "$SSH_CONNECTION" && -z "$TMUX" && "$TERM_PROGRAM" != "vscode" && -z "$NEPTUNE_FETCH_SHOWN" ]]; then
-  export NEPTUNE_FETCH_SHOWN=1
+if (( $+commands[lunafetch] )) && [[ -o interactive && -o login && -z "$SSH_CONNECTION" && -z "$TMUX" && "$TERM_PROGRAM" != "vscode" ]]; then
   lunafetch
 fi
